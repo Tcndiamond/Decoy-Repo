99bf3b2ba99a55d6288a7b036bd812e037b88db7
Conquerant Lembindi <106309388+Tcndiamond@users.noreply.github.com>
2024-04-06 21:47:20 +0200

    First Commit - Completed Code

---
+++ CodesDoneINClass.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+t = np.arange(0, 5, step = 0.01)
+y = 3*np.sin(4*np.pi*t)
+
+plt.figure()
+plt.plot(t,y)
+plt.xlabel("Time in seconds")
+plt.ylabel("Amplitude")
+plt.title("Sinewave of 2Hz")
+plt.show()
